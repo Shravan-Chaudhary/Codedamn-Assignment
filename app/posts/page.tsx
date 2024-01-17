8703c8712d32e0a92e4ca66c08d919b20e76bea7
Shravan <shravansinghchaudhary@gmail.com>
2024-01-17 12:05:42 +0530

    add header component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 const PostsPage = async () => {
-  const response = await fetch("http://localhost:1337/api/posts/1", {
-    cache: "no-store",
-  });
-  const post = await response.json();
-
-  return (
-    <MaxWidthWrapper>
-      <div>PostsPage</div>
-      <h2>{post.title}</h2>
-      <Image src={post.imageUrl} alt="image" height={200} width={200} />
-    </MaxWidthWrapper>
-  );
+  return <MaxWidthWrapper>Your Posts will render here</MaxWidthWrapper>;
 };
 
 export default PostsPage;
